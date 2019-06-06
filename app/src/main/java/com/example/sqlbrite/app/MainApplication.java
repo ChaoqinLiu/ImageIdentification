67e11c5596d8f27fa9fe0cd682bf0f6fd0a1fde3
ChaoqinLiu <1026191382@qq.com>
2019-06-06 15:43:11 +0800

    update

---
+++ app/src/main/java/com/example/sqlbrite/app/MainApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlbrite.app;
+
+import android.app.Application;
+import android.support.multidex.MultiDex;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        MultiDex.install(this);
+    }
+}
